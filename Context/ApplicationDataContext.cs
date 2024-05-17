855a3be0fb3f8ac3b00fdf3cbf28ac5832fc4e99
MiguelTiezi0 <migtiezi0007@gmail.com>
2024-05-17 12:32:35 -0300

    First Commit

---
+++ Context/ApplicationDataContext.cs
@@ -0,0 +1,15 @@
+﻿using AMST4_Carousel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMST4_Carousel.Context
+{
+    public class ApplicationDataContext: DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> opts) :base(opts)
+        {
+        
+        }
+        public DbSet<Category> Category { get; set; } 
+        public DbSet<AMST4_Carousel.Models.Product> Product { get; set; } = default!;
+    }
+}
